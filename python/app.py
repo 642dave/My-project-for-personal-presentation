ca46c73bad2daa40428f0f51a062df11f7c8d432
David Rakovsky <david.rakovsky@centrum.cz>
2024-04-19 18:14:40 +0200

    connection of the registration form to the casino_database

--- python/app.py
+++ python/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from templates.classes import Player
 
 app = Flask(__name__)
 
@@ -14,6 +15,19 @@ def registry():
 def blackjack():
     return render_template('blackjack.html')
 
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    surname = request.form['surname']
+    age = request.form['age']
+    email = request.form['email']
+    password = request.form['password']
+
+    player = Player(name, surname, age, email, password)
+    player.insert_casino_player()
+
+    return 'Registration successful!', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
+
