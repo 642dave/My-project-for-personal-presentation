38882500f50e2fb5092202b88d6d08eee9c5ad9a
David Rakovsky <david.rakovsky@centrum.cz>
2024-04-20 17:23:48 +0200

    function insert_casino_player in class Player treated with TRY EXCEPT blocks

--- python/templates/classes.py
+++ python/templates/classes.py
@@ -8,17 +8,24 @@ class Player:
         self.email = email
         self.password = password
 
-    def insert_casino_player(self):    
-        conn = psycopg2.connect(
-            dbname="casino_database",
-            user = "postgres",
-            password = "postgres",
-            host = "localhost",
-            port = "5433"
-        )
+    def insert_casino_player(self):
+        try:    
+            conn = psycopg2.connect(
+                dbname="casino_database",
+                user = "postgres",
+                password = "postgres",
+                host = "localhost",
+                port = "5433"
+            )
 
-        cur = conn.cursor()
-        query = '''INSERT INTO casino_player (name, surname, age, email, password) VALUES (%s, %s, %s, %s, %s)'''
-        cur.execute(query, (self.name, self.surname, self.age, self.email, self.password))
-        conn.commit()
-        conn.close()
+            cur = conn.cursor()
+            query = '''INSERT INTO casino_player (name, surname, age, email, password) VALUES (%s, %s, %s, %s, %s)'''
+            cur.execute(query, (self.name, self.surname, self.age, self.email, self.password))
+            conn.commit()
+            conn.close()
+        except psycopg2.DatabaseError as e:
+            print(f'Database error {e}')
+        except Exception as e:
+            print(f'General error {e}')
+        else:
+            print('User successfully added')
